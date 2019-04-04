dc8f9c46d2217de59e278f4e892d2f2f688b5452
Richard Nguyen <richard.ng55@hotmail.com>
2019-04-03 21:50:22 -0700

    added firebase connection to cardswipe

--- app/src/main/java/com/mobile/letsbone/CardSwipe.java
+++ app/src/main/java/com/mobile/letsbone/CardSwipe.java
@@ -3,7 +3,6 @@ package com.mobile.letsbone;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,7 +11,16 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.lorentzos.flingswipe.SwipeFlingAdapterView;
+import com.mobile.letsbone.ProfileData;
+import com.mobile.letsbone.R;
 
 import java.util.ArrayList;
 
@@ -21,6 +29,11 @@ public class CardSwipe extends Fragment {
     private ArrayAdapter<String> arrayAdapter;
     private PictureArrayAdapter mAdapter;
     private int i;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference databaseReference;
+    private String currentUserKey;
+    ArrayList<ProfileData> al;
 
     @Nullable
     @Override
@@ -35,32 +48,21 @@ public class CardSwipe extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         getActivity().setTitle("Match");
 
-        //add the view via xml or programmatically
-        SwipeFlingAdapterView flingContainer =(SwipeFlingAdapterView) getView().findViewById(R.id.cardFrame) ;
-
-        final ArrayList<ProfileData> al = new ArrayList<>();
-        //al.add(new ProfileData("Bob", "25","Vancouver, BC", R.drawable.dog1));
-        //al.add(new ProfileData("Joe", "21","Surrey, BC", R.drawable.dog2));
-        //al.add(new ProfileData("Buddy", "27","Coquitlam, BC", R.drawable.dog3));
-        //al.add(new ProfileData("Tyson", "19","Surrey, BC", R.drawable.dog4));
-        //al.add(new ProfileData("Tyrone", "30","Vancouver, BC", R.drawable.dog5));
-        al.add(new ProfileData("Bob", "Sura", "M", "25", "Vancouver, BC",
-                5, R.drawable.dog6, "F"));
-        al.add(new ProfileData("Joe", "Dumars", "M", "21", "Surrey, BC",
-                10, R.drawable.dog6, "F"));
-        al.add(new ProfileData("Buddy", "Hield", "M", "27", "Coquitlam, BC",
-                8, R.drawable.dog6, "F"));
-        al.add(new ProfileData("Blake", "Lively", "F", "27", "Burnaby, BC",
-                15, R.drawable.dog6, "M"));
-        al.add(new ProfileData("Anna", "Kendrick", "F", "26", "New Westminster, BC",
-                11, R.drawable.dog6, "M"));
+        //pulling data from Firebase
+        mAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("Users");
+        currentUserKey = mAuth.getCurrentUser().getUid();
 
 
+        al = new ArrayList<>();
 
+        genderCheck();
 
         //choose your favorite adapter
 
-        mAdapter = new PictureArrayAdapter(getActivity(), al);
+        mAdapter = new PictureArrayAdapter(getActivity(), R.layout.card_item, al);
+
+        SwipeFlingAdapterView flingContainer =(SwipeFlingAdapterView) getView().findViewById(R.id.cardFrame) ;
 
         //set the listener and the adapter
         flingContainer.setAdapter(mAdapter);
@@ -73,26 +75,41 @@ public class CardSwipe extends Fragment {
                 mAdapter.notifyDataSetChanged();
             }
 
+            //When user swipes left on card
             @Override
             public void onLeftCardExit(Object dataObject) {
-                //Do something on the left!
-                //You also have access to the original object.
-                //If you want to use it just cast it (String) dataObject
-                Toast.makeText(getActivity(), "Dislike!", Toast.LENGTH_SHORT).show();
+                ProfileData profileObject = (ProfileData)dataObject;
+                String userKey = profileObject.getUserId();
+
+                //creates a child No for opposite users with currents users key set to true
+                databaseReference
+                        .child(userKey)
+                        .child("Connections")
+                        .child("No")
+                        .child(currentUserKey)
+                        .setValue(true);
             }
 
+            //When users swipe right on card
             @Override
             public void onRightCardExit(Object dataObject) {
-                Toast.makeText(getActivity(), "Like!", Toast.LENGTH_SHORT).show();
+                ProfileData profileObject = (ProfileData) dataObject;
+                String userKey = profileObject.getUserId();
+
+                //creates a child Yes for opposite users with currents user set to true
+                databaseReference
+                        .child(userKey)
+                        .child("Connections")
+                        .child("Yes")
+                        .child(currentUserKey)
+                        .setValue(true);
+
+                matchUsers(userKey);
             }
 
             @Override
             public void onAdapterAboutToEmpty(int itemsInAdapter) {
-                // Ask for more data here
-                /*al.add(new ProfileData("No More Swipes", null, null, null, null, 0, 0, null));
-                mAdapter.notifyDataSetChanged();
-                Log.d("LIST", "notified");
-                i++;*/
+
                 return;
             }
 
@@ -109,4 +126,119 @@ public class CardSwipe extends Fragment {
         super.onActivityCreated(savedInstanceState);
 
     }
+
+
+    private String usersGender;
+    private String oppositeUserSex;
+    // check the current users gender
+    public void genderCheck(){
+        String userId = mAuth.getCurrentUser().getUid();
+        DatabaseReference user =  databaseReference.child(userId);
+
+
+        user.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    if(dataSnapshot.child("Gender").getValue()!= null){
+                        usersGender = dataSnapshot.child("Gender").getValue().toString();
+                        switch (usersGender){
+                            case "Male":
+                                oppositeUserSex ="Female";
+                                break;
+                            case "Female":
+                                oppositeUserSex = "Male";
+                                break;
+                        }
+                    }
+                    addUserCards();
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
+    }//end of method
+
+    //pull users from firebase based on gender
+    public  void addUserCards(){
+        databaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                if(dataSnapshot.exists() && dataSnapshot.child("Gender").getValue().toString().equals(oppositeUserSex)
+                        && !dataSnapshot.child("connections").child("No").hasChild(currentUserKey)
+                        && !dataSnapshot.child("connections").child("Yes").hasChild(currentUserKey)){
+
+                    al.add(new ProfileData(dataSnapshot.getKey(),
+                            dataSnapshot.child("FirstName").getValue().toString(),
+                            dataSnapshot.child("LastName").getValue().toString(),
+                            dataSnapshot.child("Gender").getValue().toString(),
+                            dataSnapshot.child("Age").getValue().toString(),
+                            "New WestMinister",
+                            11,
+                            R.drawable.dog6,
+                            "M"));
+
+                    mAdapter.notifyDataSetChanged();
+                }
+
+            }
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+            }
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+            }
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+    }// end of addUserCards method
+
+    //connect users that liked each other
+    private void matchUsers(String userKey){
+        DatabaseReference userConnections = databaseReference.child(currentUserKey).child("Connections").child("Yes").child(userKey);
+
+        userConnections.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()) {
+
+                    String messageId = FirebaseDatabase.getInstance().getReference().child("Messages").push().getKey();
+
+                    databaseReference
+                            .child(dataSnapshot.getKey())
+                            .child("Connections")
+                            .child("Matches")
+                            .child(currentUserKey)
+                            .child("MessageId")
+                            .setValue(messageId);
+
+                    databaseReference
+                            .child(currentUserKey)
+                            .child("Connections")
+                            .child("Matches")
+                            .child(dataSnapshot.getKey())
+                            .child("MessageId")
+                            .setValue(messageId);
+
+                    Toast.makeText(getActivity(), "New Connection", Toast.LENGTH_SHORT).show();
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }//end of matchUser method
+
 }
--- app/src/main/java/com/mobile/letsbone/PictureArrayAdapter.java
+++ app/src/main/java/com/mobile/letsbone/PictureArrayAdapter.java
@@ -2,7 +2,6 @@ package com.mobile.letsbone;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.media.Image;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -13,40 +12,53 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.mobile.letsbone.ProfileData;
+import com.mobile.letsbone.R;
+
 import java.util.ArrayList;
-import java.util.List;
 
 public class PictureArrayAdapter  extends ArrayAdapter<ProfileData> {
 
     private Context mContext;
-    private List<ProfileData> profileList = new ArrayList<>();
 
 
-    public PictureArrayAdapter(@NonNull Context context, @SuppressLint("SupportAnnotationUsage") @LayoutRes ArrayList<ProfileData> list) {
-        super(context, 0, list);
-        mContext = context;
-        profileList = list;
+
+    public PictureArrayAdapter(@NonNull Context context, int resourceId, @SuppressLint("SupportAnnotationUsage") @LayoutRes ArrayList<ProfileData> list) {
+        super(context, resourceId, list);
+
     }
 
 
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
-        View listItem = convertView;
+        ProfileData profile_item = getItem(position);
 
+        if(convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.card_item, parent,false);
+        }
+
+        ImageView image =(ImageView)convertView.findViewById(R.id.picture);
+        image.setImageResource(profile_item.getmPhoto());
+
+        TextView name = (TextView)convertView.findViewById(R.id.profileName);
+        name.setText(profile_item.getfName() + " " + profile_item.getlName());
+
+        TextView age = (TextView)convertView.findViewById(R.id.profileAge);
+        age.setText(profile_item.getAge());
+
+        return convertView;
+
+        /*
         if(listItem == null)
             listItem = LayoutInflater.from(mContext).inflate(R.layout.card_item,parent,false);
-
         ProfileData currentProfile  = profileList.get(position);
-
         ImageView image =(ImageView)listItem.findViewById(R.id.picture);
         image.setImageResource(currentProfile.getmPhoto());
-
         TextView name = (TextView)listItem.findViewById(R.id.profileName);
         name.setText(currentProfile.getfName() + " " + currentProfile.getlName());
-
         TextView age = (TextView)listItem.findViewById(R.id.profileAge);
         age.setText(currentProfile.getAge());
-
         return listItem;
+        */
     }
 
 }
